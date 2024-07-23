475ff3625b26145bcb72691e3783c44c5c4dde04
dntjd129 <dntjd129@naver.com>
2024-07-23 14:49:35 +0900

    💄 header css

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "@assets/scss/style.scss";
+
+const Header = () => {
+  return (
+    <>
+      <article id="header">
+        <div className="top_banner">
+          <a href="#">
+            <span color="yellow">YONEX TENNIS COLLECTION</span>
+          </a>
+        </div>
+        <div id="header_sub"></div>
+      </article>
+    </>
+  );
+};
+
+export default Header;
