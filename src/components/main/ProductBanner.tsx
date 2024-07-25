1683ad99b9c6fea08c152a8650c0ebb510c32273
dntjd129 <dntjd129@naver.com>
2024-07-25 11:13:28 +0900

    ✨ banner-box 컴포넌트 추가

--- src/components/main/ProductBanner.tsx
+++ src/components/main/ProductBanner.tsx
@@ -1,71 +1,43 @@
 import React from "react";
 import "../../assets/scss/style.scss";
+import badminton from "../../assets/img/banner_box/banner_badminton.png";
+import bag_acc from "../../assets/img/banner_box/banner_bag_acc.png";
+import golf from "../../assets/img/banner_box/banner_golf.png";
+import tennis from "../../assets/img/banner_box/banner_tennis.png";
+import wears from "../../assets/img/banner_box/banner_wears.png";
+import korea_team from "../../assets/img/banner_box/icon_korea_team.jpg";
 
 const ProductBanner = () => {
+  const bannerImg = [badminton, bag_acc, golf, tennis, wears];
+  const bannerText = ["BADMINTON", "BAG & ACC", "GOLF", "TENNIS", "WEARS"];
+
   return (
-    <>
-      <div className="section">
-        <div className="section_wrap">
-          <div className="section_title">
-            <h2 className="title">
-              <span className="title">YONEX KOREA</span>
-            </h2>
-            <div className="divider"></div>
-            <p className="title__sub">요넥스의 다양한 상품들을 만나보세요.</p>
-          </div>
-        </div>
-        <div className="section_wrap">
-          <div className="banner-box">
-            <a href="">
-              <img src="" alt="" />
-              <div>
-                <p>BADMINTON</p>
-              </div>
-            </a>
-          </div>
-          <div className="banner-box">
-            <a href="">
-              <img src="" alt="" />
-              <div>
-                <p>TENNIS</p>
-              </div>
-            </a>
-          </div>
-          <div className="banner-box">
-            <a href="">
-              <img src="" alt="" />
-              <div>
-                <p>GOLF</p>
-              </div>
-            </a>
-          </div>
-          <div className="banner-box">
-            <a href="">
-              <img src="" alt="" />
-              <div>
-                <p>WEARS</p>
-              </div>
-            </a>
-          </div>
-          <div className="banner-box">
-            <a href="">
-              <img src="" alt="" />
-              <div>
-                <p>BADMINTON</p>
-              </div>
-            </a>
-          </div>
-          <div className="banner-box">
-            <a href="">
-              <img src="" alt="" />
-              <div>
-                <p>BADMINTON</p>
-              </div>
-            </a>
-          </div>
+    <div className="section">
+      <div className="section_wrap">
+        <div className="section_title">
+          <h2 className="title">
+            <span className="title">YONEX KOREA</span>
+          </h2>
+          <div className="divider"></div>
+          <p className="title__sub">요넥스의 다양한 상품들을 만나보세요.</p>
         </div>
       </div>
-    </>
+      <div className="section_wrap">
+        {bannerImg.map((imgSrc, index) => (
+          <div className="banner-box" key={index}>
+            <a href="#">
+              <img src={imgSrc} alt={bannerText[index]} />
+              <div>{/* <p>{bannerText[index]}</p> */}</div>
+            </a>
+          </div>
+        ))}
+        <div className="banner-box-korea">
+          <a href="">
+            <img src={korea_team} />
+          </a>
+        </div>
+      </div>
+    </div>
   );
 };
 
