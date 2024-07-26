73039ae11397c4dfda5d4c874730f2af2ade30c3
dntjd129 <dntjd129@naver.com>
2024-07-26 18:03:19 +0900

    ✨ 라우터 수정

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,7 +1,39 @@
 import React from "react";
+import "../../assets/scss/style.scss";
+import Header from "../../components/header/Header";
+import SlideCard from "../../components/main/SlideCard";
+import ProductBanner from "../../components/main/ProductBanner";
+import MdPick from "../../components/main/MdPick";
 
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <>
+      <section id="page_container" className="isTopBanner">
+        {/* navbar */}
+        <Header />
+        {/* 메인 swiper */}
+        <SlideCard />
+        {/* 메인 취급 물품 배너 */}
+        <ProductBanner />
+        {/* MD's Pick */}
+        <MdPick />
+        {/* 회원 전용 혜택 */}
+        <div></div>
+        {/* 요넥스 소식 */}
+        <div></div>
+        {/* SNS */}
+        <div></div>
+        {/* 바로가기 */}
+        <div></div>
+        {/* notice */}
+        <div></div>
+        {/* notice-panel */}
+        <div></div>
+        {/* footer */}
+        <div></div>
+      </section>
+    </>
+  );
 };
 
 export default Main;
